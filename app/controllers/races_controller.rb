437418526ee171ee0bac61d40fa030c20c9ffcc6
Luis Muñoz <l.munoz.m@outlook.com>
2024-11-09 17:12:27 -0300

    Update races_controller.rb
    Fix time zone name.

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class RacesController < ApplicationController
-  def new; end
+  def new
+  end
 
   def time_registration
     @race = Race.includes(:checkpoints).find(params[:race_id])
@@ -16,7 +17,7 @@ class RacesController < ApplicationController
   def start
     @race = Race.includes(:checkpoints).find(params[:race_id])
     @race.update(
-      start_datetime: helpers.timestamp_to_time(params[:start_time]).to_datetime.in_time_zone("Chile/Continental")
+      start_datetime: helpers.timestamp_to_time(params[:start_time]).to_datetime.in_time_zone('America/Santiago'),
     )
   end
 
@@ -24,7 +25,7 @@ class RacesController < ApplicationController
     @race = Race.includes(:checkpoints).find(params[:race_id])
     add_checkpoint(@race, params)
     @race.update(
-      end_datetime: helpers.timestamp_to_time(params[:end_time]).to_datetime.in_time_zone("Chile/Continental")
+      end_datetime: helpers.timestamp_to_time(params[:end_time]).to_datetime.in_time_zone('America/Santiago'),
     )
   end
 
@@ -40,33 +41,34 @@ class RacesController < ApplicationController
   end
 
   private
-    def add_checkpoint(race, params)
-      race.checkpoints.create(
-        start: helpers.timestamp_to_time(params[:start_checkpoint]).to_datetime.in_time_zone("Chile/Continental"),
-        end: helpers.timestamp_to_time(params[:end_checkpoint]).to_datetime.in_time_zone("Chile/Continental"),
-        arm_start:
-          (
-            if params[:arm_start] == "null"
-              nil
-            else
-              helpers.timestamp_to_time(params[:arm_start]).to_datetime.in_time_zone("Chile/Continental")
-            end
-          ),
-        arm_end:
-          (
-            if params[:arm_end] == "null"
-              nil
-            else
-              helpers.timestamp_to_time(params[:arm_end]).to_datetime.in_time_zone("Chile/Continental")
-            end
-          ),
-        elapsed_time:
-          helpers.timestamp_to_time(params[:end_checkpoint]).to_datetime.in_time_zone("Chile/Continental") -
-            helpers.timestamp_to_time(params[:start_checkpoint]).to_datetime.in_time_zone("Chile/Continental")
-      )
-    end
 
-    def race_params
-      params.permit(:start, :end, :pool_type, :proof_type, :style, :distance)
-    end
+  def add_checkpoint(race, params)
+    race.checkpoints.create(
+      start: helpers.timestamp_to_time(params[:start_checkpoint]).to_datetime.in_time_zone('America/Santiago'),
+      end: helpers.timestamp_to_time(params[:end_checkpoint]).to_datetime.in_time_zone('America/Santiago'),
+      arm_start:
+        (
+          if params[:arm_start] == 'null'
+            nil
+          else
+            helpers.timestamp_to_time(params[:arm_start]).to_datetime.in_time_zone('America/Santiago')
+          end
+        ),
+      arm_end:
+        (
+          if params[:arm_end] == 'null'
+            nil
+          else
+            helpers.timestamp_to_time(params[:arm_end]).to_datetime.in_time_zone('America/Santiago')
+          end
+        ),
+      elapsed_time:
+        helpers.timestamp_to_time(params[:end_checkpoint]).to_datetime.in_time_zone('America/Santiago') -
+          helpers.timestamp_to_time(params[:start_checkpoint]).to_datetime.in_time_zone('America/Santiago'),
+    )
+  end
+
+  def race_params
+    params.permit(:start, :end, :pool_type, :proof_type, :style, :distance)
+  end
 end
