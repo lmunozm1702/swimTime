bb4c959555ab2bb1653f1f7a7f05eb9f87bc3667
Luis Muñoz <l.munoz.m@outlook.com>
2024-12-27 18:25:40 -0300

    remove proofs

--- db/migrate/20240620222900_remove_column_proofs_id_from_races.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveColumnProofsIdFromRaces < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :races, :proofs_id
-  end
-end
--- db/migrate/20240621131144_drop_table_proofs.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class DropTableProofs < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :proofs
-  end
-end
